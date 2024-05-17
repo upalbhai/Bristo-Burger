f6a8684c6481df822dcc9e1ee0e45be8cada9f03
upalbhai <108395707+upalbhai@users.noreply.github.com>
2024-05-18 03:03:46 +0530

    Update provider.js

--- server/utils/provider.js
+++ server/utils/provider.js
@@ -8,7 +8,7 @@ const connectPassport = () => {
     passport.use(new GoogleStrategy({
         clientID: process.env.GOOGLE_CLIENT_ID,
         clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-        callbackURL: process.env.GOOGLE_CALLBACK_URL,
+        callbackURL: "https://bristo-burger.onrender.com/api/v1/login",
     }, async function(accessToken, refreshToken, profile, done) {
         // database comes here
         const user = await User.findOne({
