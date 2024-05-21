6fcab90c3efc67f76b96dd74cd7a62fa778bfaba
upalbhai <108395707+upalbhai@users.noreply.github.com>
2024-05-21 16:51:09 +0530

    Update store.js

--- server/client/src/redux/store.js
+++ server/client/src/redux/store.js
@@ -15,4 +15,4 @@ const store= configureStore({
 
 export default store;
 
-export const server="http://bristo-burger.onrender.com/api/v1"
+export const server="https://bristo-burger.onrender.com/api/v1"
